50ae1c9041f3c005ab38519160d7f9e13904fde4
KindTis <tatis3@gmail.com>
2020-06-08 19:55:30 +0900

    설정 디렉토리 파싱 및 커버 이미지 업데이트

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,65 +27,131 @@ namespace Airi
     /// </summary>
     public partial class MainWindow
     {
+        HtmlWeb mWeb = new HtmlWeb();
+        private List<CompareListItem> mThumbnailList = new List<CompareListItem>();
+        private BackgroundWorker mDownloadWorker = new BackgroundWorker();
+        private Border mPrevSelectedBorder = null;
+
         public class CompareListItem
         {
+            // binded
             public string strImagePath { get; set; }
             public string strTitle { get; set; }
-        }
 
-        private List<CompareListItem> mThumbnailList = new List<CompareListItem>();
+            // ext
+            public string fullPath { get; set; }
+        }        
+
         public MainWindow()
         {
             InitializeComponent();
             Directory.CreateDirectory("thumb");
             lbThumbnailList.ItemsSource = mThumbnailList;
-            ShowImage();
+
+            mDownloadWorker.WorkerReportsProgress = true;
+            mDownloadWorker.WorkerSupportsCancellation = true;
+            mDownloadWorker.DoWork += new DoWorkEventHandler(_DoWork);
+            mDownloadWorker.RunWorkerAsync();
+            //File.WriteAllText(@"list.json", JsonConvert.SerializeObject(mThumbnailList));
         }
 
-        private void ShowImage()
+        public void _DoWork(object sender, DoWorkEventArgs e)
         {
-            for (int i = 0; i < 100; i++)
+            _ParseDirectory(@"e:\");
+            _UpdateCoverImg();
+        }
+
+        private void _ParseDirectory(string path)
+        {
+            string[] fileEntries = Directory.GetFiles(path);
+            foreach (string fileName in fileEntries)
             {
                 mThumbnailList.Add(new CompareListItem()
                 {
-                    strImagePath = System.IO.Path.GetFullPath(@"thumb/img.jpg"),
-                    strTitle = "Title"
+                    strImagePath = System.IO.Path.GetFullPath(@"thumb/noimage.jpg"),
+                    strTitle = System.IO.Path.GetFileNameWithoutExtension(fileName),
+                    fullPath = fileName
                 });
             }
-            lbThumbnailList.Items.Refresh();
+
+            string[] subdirectoryEntries = Directory.GetDirectories(path);
+            foreach (string subdirectory in subdirectoryEntries)
+                _ParseDirectory(subdirectory);
         }
 
-        private FrameworkElement FindByName(string name, FrameworkElement root)
+        private void _UpdateCoverImg()
         {
-            Stack<FrameworkElement> tree = new Stack<FrameworkElement>();
-            tree.Push(root);
-
-            while (tree.Count > 0)
+            foreach (var e in mThumbnailList)
             {
-                FrameworkElement current = tree.Pop();
-                if (current.Name == name)
-                    return current;
-
-                int count = VisualTreeHelper.GetChildrenCount(current);
-                for (int i = 0; i < count; ++i)
+                var html = @"http://www.javlibrary.com/en/vl_searchbyid.php?keyword=" + e.strTitle;
+                var htmlDoc = mWeb.Load(html);
+                if (_ParsingHTMLPage(htmlDoc.DocumentNode, e.strTitle))
                 {
-                    DependencyObject child = VisualTreeHelper.GetChild(current, i);
-                    if (child is FrameworkElement)
-                        tree.Push((FrameworkElement)child);
+                    e.strImagePath = System.IO.Path.GetFullPath(@"thumb/" + e.strTitle + @".jpg");
+                }
+
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    lbThumbnailList.Items.Refresh();
+                }));
+            }
+        }
+
+        private bool _ParsingHTMLPage(HtmlNode node, string name)
+        {
+            var selectNode = node.SelectSingleNode("//text()[contains(., 'ID Search Result')]/..");
+            if (selectNode != null)
+            {
+                selectNode = node.SelectSingleNode("//div[contains(@class, 'videos')]");
+                var link = selectNode.SelectSingleNode(".//a[@href]");
+                if (link == null)
+                    return false;
+                var href = link.Attributes["href"].Value;
+                var htmlDoc = mWeb.Load(@"http://www.javlibrary.com/en/" + href);
+                return _ParsingHTMLPage(htmlDoc.DocumentNode, name);
+            }
+            else
+            {
+                selectNode = node.SelectSingleNode("//img[contains(@id, 'video_jacket_img')]");
+                var imgSrc = selectNode.Attributes["src"].Value;
+
+                try
+                {
+                    using (var imgClient = new WebClient())
+                    {
+                        imgClient.DownloadFile("http:" + imgSrc, @"thumb/" + name + @".jpg");
+                        return true;
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return false;
                 }
             }
-
-            return null;
         }
 
         private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var item = sender as ListViewItem;
-            Border b = FindByName("Outline", item) as Border;
+            Border b = Util.FindByName("Outline", item) as Border;
             b.BorderBrush = Brushes.Red;
 
+            if (mPrevSelectedBorder != null && mPrevSelectedBorder != b)
+                mPrevSelectedBorder.BorderBrush = Brushes.Transparent;
+
+            mPrevSelectedBorder = b;
+
             if (e.ClickCount == 2)
-                b.BorderBrush = Brushes.Blue;
+            {
+                new Process
+                {
+                    StartInfo = new ProcessStartInfo(mThumbnailList[lbThumbnailList.SelectedIndex].fullPath)
+                    {
+                        UseShellExecute = true
+                    }
+                }.Start();
+            }
         }
     }
 }
